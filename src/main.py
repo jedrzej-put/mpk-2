3a2f32ca45a7316d077dc879aaa6575c4b3ed0ea
jesmok <jedrzejsmok55@gmail.com>
2022-11-25 12:41:32 +0100

    add sqlite and alembic

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from .database import crud, models, schemas
+from .database.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
-async def root():
+async def root(db: Session = Depends(get_db)):
     return {"message": "Hello World"}
